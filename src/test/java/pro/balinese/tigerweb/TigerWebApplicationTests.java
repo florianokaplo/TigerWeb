21f502f6fc863fe98b4fa6ddf953520dd00da7a5
florianokaplo <florian.kaplenig.fk@gmail.com>
2024-04-19 17:58:46 +0200

    Initial commit

---
+++ src/test/java/pro/balinese/tigerweb/TigerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.balinese.tigerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
